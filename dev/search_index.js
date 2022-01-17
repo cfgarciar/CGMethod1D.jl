dd9f235cf82d2491c1d3112554a478d65079b556
Documenter.jl <documenter@juliadocs.github.io>
2022-01-17 22:24:09 +0000

    build based on ff1f040

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CGMethod1D","category":"page"},{"location":"#CGMethod1D","page":"Home","title":"CGMethod1D","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CGMethod1D.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CGMethod1D]","category":"page"}]
+}
