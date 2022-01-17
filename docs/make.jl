b4e513f32b14a1f03f69ed184ff2f36a4edba094
cfgarciar <cfgarciar@unal.edu.co>
2022-01-17 18:20:38 -0500

    Docs modification

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,21 @@
 using CGMethod1D
-using Documenter
+using Documenter, Pkg
 
-DocMeta.setdocmeta!(CGMethod1D, :DocTestSetup, :(using CGMethod1D); recursive=true)
+Pkg.precompile()
 
-makedocs(;
-    modules=[CGMethod1D],
-    authors="Christian Garcia",
-    repo="https://github.com/cfgarciar/CGMethod1D.jl/blob/{commit}{path}#{line}",
+const is_ci = haskey(ENV, "GITHUB_ACTIONS")
+
+makedocs(
+    format=Documenter.HTML(prettyurls=is_ci),
     sitename="CGMethod1D.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://cfgarciar.github.io/CGMethod1D.jl",
-        assets=String[],
-    ),
-    pages=[
+    doctest = false,
+    strict = false,
+    pages=Any[
         "Home" => "index.md",
     ],
 )
 
 deploydocs(;
     repo="github.com/cfgarciar/CGMethod1D.jl",
-    devbranch="master",
+    push_preview=true,
 )
