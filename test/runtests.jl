73a709b104e0f45c1dc37f0ece059b80ee9ad366
cfgarciar <cfgarciar@unal.edu.co>
2022-01-18 09:47:54 -0500

    test files updated

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,7 @@
 using CGMethod1D
 using Test
 
-<<<<<<< HEAD
 
 include("test_interpolation.jl")
 include("test_quadrature.jl")
 
-=======
-# include("test_init.jl")
-include("test_interpolation.jl")
->>>>>>> master
