6f40ffcbb38926dbfffe45f0feb89fff62e1fcac
cfgarciar <cfgarciar@unal.edu.co>
2022-01-17 21:47:00 -0500

    Test interpolation file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using CGMethod1D
 using Test
 
 include("test_init.jl")
+include("test_interpolation.jl")
---
+++ test/test_interpolation.jl
@@ -0,0 +1,4 @@
+# Test Interpolation
+@testset "lagrange" begin
+    @test isapprox(2+2, 4, atol=1e-8)
+end
