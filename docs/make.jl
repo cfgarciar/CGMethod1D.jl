538291161ba1ffcd0e7a1b76f75cdeed3837e777
cfgarciar <cfgarciar@unal.edu.co>
2022-01-17 19:41:08 -0500

    Docs index

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,3 @@
-# using CGMethod1D
-# using Documenter, Pkg
-
-# Pkg.precompile()
-
-# const is_ci = haskey(ENV, "GITHUB_ACTIONS")
-
-# makedocs(
-#     format=Documenter.HTML(prettyurls=is_ci),
-#     sitename="CGMethod1D.jl",
-#     doctest = false,
-#     strict = false,
-#     pages=Any[
-#         "Home" => "index.md",
-#     ],
-# )
-
-# deploydocs(;
-#     repo="github.com/cfgarciar/CGMethod1D.jl",
-#     push_preview=true,
-# )
 
 using CGMethod1D
 using Documenter, Pkg
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,18 +1,3 @@
-<!-- ```@meta
-CurrentModule = CGMethod1D
-```
-
-# CGMethod1D
-
-Documentation for [CGMethod1D](https://github.com/cfgarciar/CGMethod1D.jl).
-
-```@index
-```
-
-```@autodocs
-Modules = [CGMethod1D]
-``` -->
-
 
 
 # CGMethod1D.jl
